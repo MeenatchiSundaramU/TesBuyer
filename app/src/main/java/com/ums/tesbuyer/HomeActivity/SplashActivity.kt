492e201dbb57ca14038cec38957d30b6109d7ff0
Mano <meenatchisundaram63@gmail.com>
2021-02-03 09:38:00 -0800

    Second Commit

--- app/src/main/java/com/ums/tesbuyer/HomeActivity/SplashActivity.kt
+++ app/src/main/java/com/ums/tesbuyer/HomeActivity/SplashActivity.kt
@@ -14,6 +14,7 @@ import com.ums.tesbuyer.Authentication.SignInActivity
 import com.ums.tesbuyer.R
 
 class SplashActivity : AppCompatActivity() {
+    //Hello
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
